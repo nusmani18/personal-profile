5c87004e6b9b991d3ee2dc89c499b1f5a4f2f2be
nusmani18 <95830302+nusmani18@users.noreply.github.com>
2021-12-08 15:29:40 -0800

    Add files via upload

---
+++ firstpage.js
@@ -0,0 +1,10 @@
+var age = document.getElementsByName("age");
+
+function check() {
+  if (age.value = "14") {
+      document.write("You got it right!");
+    }
+    else {
+      document.write("You got it wrong!");
+  }
+}
